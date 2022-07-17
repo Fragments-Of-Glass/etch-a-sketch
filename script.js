5c147cbd71f13f934183ae99eb27d5b4875fe851
Jonathan-Zim <2000yoni11@gmail.com>
2022-07-17 16:02:30 +0300

    Add ability to resize grid

--- script.js
+++ script.js
@@ -1,22 +1,61 @@
 let container = document.getElementById("container");
 container.style.display = 'grid';
-container.style.gridTemplateColumns = `repeat(16, 1fr)`;
-container.style.gridTemplateRows = `repeat(16, 1fr)`;
 let cell;
 let color = [];
 var eraser = false;
 var coloring = true;
 let height;
+var div;
+const btn = document.getElementById('info');
+const resetbtn = document.getElementById('reset');
+const erasebtn = document.getElementById('erase');
+const colorbtn = document.getElementById('color');
 makeGrid();
 function makeGrid() {
+    container.style.gridTemplateColumns = `repeat(16, 1fr)`;
+    
     for (let i = 0; i < (16 * 16); i++) {
         cell = document.createElement("div");
         container.appendChild(cell).className = 'grid-block';
         color.push(cell);
     }
     for (let i = 0; i < (16 * 16); i++) {
-        color[i].addEventListener('click', () => (changeColor(color[i])));  
+        color[i].addEventListener('click', () => (changeColor(color[i])));
     }
+}
+function makeNewGrid(rows) {
+    height = 512 / rows;
+    container.style.gridTemplateColumns = `repeat(${rows}, 1fr)`;
+    while (container.firstElementChild) {
+        color.pop(cell);
+        container.removeChild(container.lastChild);
+    }
+    for (let i = 0; i < (rows * rows); i++) {
+
+        cell = document.createElement("div");
+        container.appendChild(cell).className = 'grid-block';
+        div=document.querySelector('.grid-block');
+        div.height=height;
+        div.width=height;
+        color.push(cell);
+    }
+    for (let i = 0; i < (rows * rows); i++) {
+        color[i].addEventListener('click', () => (changeColor(color[i])));
+
+    }
+    erasebtn.addEventListener('click', () => {
+        eraser = true;
+        for (let i = 0; i < (rows * rows); i++) {
+            color[i].addEventListener('mouseover', () => erase(color[i], 1));
+        }
+        coloring = false;
+    })
+    resetbtn.addEventListener('click', () => {
+        for (let i = 0; i < (rows * rows); i++) {
+            color[i].addEventListener('click', reset(color[i]));
+        }
+    })
+
 }
 function changeColor(item) {
     if (!coloring) return;
@@ -29,10 +68,6 @@ function erase(item) {
     if (!eraser) return;
     item.style.background = 'white';
 }
-const btn = document.getElementById('info');
-const resetbtn = document.getElementById('reset');
-const erasebtn = document.getElementById('erase');
-const colorbtn = document.getElementById('color');
 colorbtn.addEventListener('click', () => {
     eraser = false;
     for (let i = 0; i < (16 * 16); i++) {
@@ -52,12 +87,11 @@ resetbtn.addEventListener('click', () => {
     for (let i = 0; i < (16 * 16); i++) {
         color[i].addEventListener('click', reset(color[i]));
     }
-}
-)
+})
 btn.addEventListener('click', () => {
     let size = parseInt(prompt('Enter new number of squares per side'));
-    if (size > 100) alert('no');
+    if (size > 100 || size < 1) alert('no');
     else {
-        makeNewGrid(size, size);
+        makeNewGrid(size);
     }
 });
